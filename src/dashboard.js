eafac69f6d3a77f8ae364e8d11dadde35079b06f
Alex Martynov <lovealex574@gmail.com>
2021-11-01 18:52:01 +0300

    Add component Keyboard(fake)

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,5 +1,6 @@
 import Message from './message';
 import './dashboard.css';
+import Keyboard from './keyboard';
 
 const userName = 'ALEX';
 const userMessage = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
@@ -19,6 +20,7 @@ function Dashboard(){
     return (
         <div className="dashboards">
             <Message name={userName} message={userMessage}/>
+            <Keyboard />
         </div>
     );
 };
